3e6c297086118c2065d119022d2d19ab910e2e47
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-12-11 20:23:53 +0200

    ad

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
-def main():
-    print("Hello, World!")
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-3-pro-preview"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""Скажи привітання українською мовою і розкажи коротко про історію України. Потім знайди в інтернеті останні новини про Україну і додай їх до відповіді."""),
+            ],
+        ),
+    ]
+    tools = [
+        types.Tool(googleSearch=types.GoogleSearch(
+        )),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        tools=tools
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
 
 if __name__ == "__main__":
-    main()
+    generate()
